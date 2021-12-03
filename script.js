c913a06a085f6d5627267046a2965f696f96d94c
Donal Munnelly <donal.munnelly@gmail.com>
2021-12-03 16:59:22 +0000

    comments added to js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // Create a preset array of numbers. Create a piece of code that chooses the largest number from that array
 let preset=[5,6,98,202,42,8,54]
 	function largest() {
+		//set the large variable to zero
 		var large=0;
+		//step through the array and compare each number with the one before first is zero
 		for(i=0;i<preset.length;i++){
 			if (preset[i] >large){
 				large=preset[i];
 			}
 		}		
-		alert("the biggest number is"+large);
+		//tell the user what the biggest figure in array is
+		alert("The biggest number is "+large);
 
 			
 	}
