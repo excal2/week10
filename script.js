42c48765f5e23a7ace59d5d54d5a84eb63a371e5
Donal Munnelly <donal.munnelly@gmail.com>
2021-12-03 17:08:59 +0000

    missed semicolon

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Create a preset array of numbers. Create a piece of code that chooses the largest number from that array
 let preset=[5,6,98,202,42,8,54]
 function showarray(){
-	document.getElementById("main").innerHTML=preset.toString()
+	document.getElementById("main").innerHTML=preset.toString();
 }
 	function largest() {
 		//set the large variable to zero
