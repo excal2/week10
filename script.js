c03dd5d84e4faba664d679eaceeecb2b4cf022d2
Donal Munnelly <donal.munnelly@gmail.com>
2021-12-03 17:07:22 +0000

    added second funtion to load array to show to user

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Create a preset array of numbers. Create a piece of code that chooses the largest number from that array
 let preset=[5,6,98,202,42,8,54]
+function showarray(){
+	document.getElementById("main").innerHTML=preset.toString()
+}
 	function largest() {
 		//set the large variable to zero
 		var large=0;
